7ea76d58c56592866f8306d1c556085cc82264df
Anthony <evansanton58@gmail.com>
2026-04-16 21:41:12 +0100

    ensured type-safe for async fetchImages() and added user feedback

--- image-slider/script1.js
+++ image-slider/script1.js
@@ -2,3 +2,23 @@ const slider = document.querySelector(".slider");
 const dotsContainer = document.querySelector(".dots-container");
 const btnPrev = document.querySelector(".btn-prev");
 const btnNext = document.querySelector(".btn-next");
+
+async function fetchImages() {
+  try {
+    const response = await fetch(
+      "https://picsum.photos/v2/list?page=5&limit=10",
+    );
+
+    if (!response.ok) throw new Error("Failed to fetch images");
+
+    const images = await response.json();
+
+    if (Array.isArray(images) && images.length > 0) {
+      showImages(images);
+    } else {
+      slider.innerHTML = `<p class="slider-error">No images found.</p>`;
+    }
+  } catch (error) {
+    console.error("Error fetching images:", error);
+  }
+}
