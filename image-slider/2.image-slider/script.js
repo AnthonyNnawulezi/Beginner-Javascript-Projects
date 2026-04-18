382d4f230d3b139b11e91d7ecd6c2ca50caa4fad
Anthony <evansanton58@gmail.com>
2026-04-18 01:32:32 +0100

    created image slider

--- image-slider/2.image-slider/script.js
+++ image-slider/2.image-slider/script.js
@@ -1,10 +1,12 @@
 const slides = document.querySelector(".slider");
 const dots = document.querySelector(".dot-container");
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
 
 async function fetchImages() {
   try {
     const response = await fetch(
-      "https://picsum.photos/v2/list?page=2&limit=100",
+      "https://picsum.photos/v2/list?page=2&limit=10",
     );
     const images = await response.json();
     console.log(images);
@@ -13,6 +15,7 @@ async function fetchImages() {
       slides.innerHTML = '<p class="error">Error fetching Images</p>';
     } else {
       renderImages(images);
+      initSlider(images);
     }
   } catch (error) {
     console.log(error, "Error fetching images");
@@ -27,7 +30,18 @@ function renderImages(images) {
     `,
     )
     .join("");
-  dots.innerHTML = images.map((image) => '<span class="dots"></span>').join("");
+  dots.innerHTML = images.map((image) => `<span class="dots"></span>`).join("");
+}
+
+function initSlider(images) {
+  const slides = document.querySelector(".slider-container");
+  const dots = document.querySelector(".dots");
+  let currentSlide = 0;
+  function goToSlide(currentSlide) {}
+  goToSlide(0);
+  btnNext.addEventListener("click", {});
+  btnPrev.addEventListener("click", {});
+  btnNext.addEventListener("click", {});
 }
 
 fetchImages();
