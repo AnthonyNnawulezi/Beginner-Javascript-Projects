6083989de7cb68cb08eab1f4c1517c6189861363
Anthony <evansanton58@gmail.com>
2026-04-22 23:39:26 +0100

    added and designed loading state for UX

--- load-more-api/script1.js
+++ load-more-api/script1.js
@@ -1,2 +1,13 @@
 const productsContainer = document.querySelector(".products-container");
 const loadMoreBtn = document.getElementById("loadMoreBtn");
+
+let currentPage = 0;
+const LIMIT = 10;
+let isLoading = false;
+
+async function fetchProducts(page) {
+  if (isFetching) return;
+  isFetching = true;
+  loadMoreBtn.textContent = "Loading...";
+  loadMoreBtn.disabled = true;
+}
