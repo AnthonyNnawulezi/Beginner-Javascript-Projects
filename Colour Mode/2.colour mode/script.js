87d2678cb61c0b11ddabb421d176959187fad775
Anthony <evansanton58@gmail.com>
2026-04-29 18:53:06 +0100

    finalised the 2nd version functionality

--- Colour Mode/2.colour mode/script.js
+++ Colour Mode/2.colour mode/script.js
@@ -1,1 +1,14 @@
 const toggleBtn = document.querySelector(".toggle-btn");
+const body = document.body;
+const themes = ["dark", "light", "blue", "brown", "grey"];
+
+let currentTheme = localStorage.getItem("theme") || themes[0];
+
+body.setAttribute("data-theme", currentTheme);
+
+toggleBtn.addEventListener("click", () => {
+  currentIndex = themes.indexOf(currentTheme);
+  currentTheme = themes[(currentIndex + 1) % themes.length];
+  body.setAttribute("data-theme", currentTheme);
+  localStorage.setItem("theme", currentTheme);
+});
