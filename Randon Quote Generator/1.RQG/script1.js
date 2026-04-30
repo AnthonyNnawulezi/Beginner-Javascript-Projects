79ddfe9098d6b6df23abd5d42aec2a9d111e5196
Anthony <evansanton58@gmail.com>
2026-04-30 13:57:52 +0100

    Added 2 versions of toggleloader

--- Randon Quote Generator/1.RQG/script1.js
+++ Randon Quote Generator/1.RQG/script1.js
@@ -6,3 +6,14 @@ if (!wrapper || !button || !loader) {
   console.error("Required DOM elements are missing. Check your HTML.");
   return;
 }
+
+function toggleLoader(show) {
+  loader.classList.toggle("hidden", !show);
+  wrapper.classList.toggle("hidden", show);
+}
+
+//or
+function toggleLoader(show) {
+  loader.style.display = show ? "block" : "none";
+  wrapper.style.display = show ? "none" : "block";
+}
