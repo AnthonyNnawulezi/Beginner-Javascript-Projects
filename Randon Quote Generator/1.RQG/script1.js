66ebf310f09a346c038f4b473142205286d4302b
Anthony <evansanton58@gmail.com>
2026-04-30 14:39:22 +0100

    used destructuring for showQuote()

--- Randon Quote Generator/1.RQG/script1.js
+++ Randon Quote Generator/1.RQG/script1.js
@@ -13,7 +13,27 @@ function toggleLoader(show) {
 }
 
 //or
-function toggleLoader(show) {
-  loader.style.display = show ? "block" : "none";
-  wrapper.style.display = show ? "none" : "block";
+// function toggleLoader(show) {
+//   loader.style.display = show ? "block" : "none";
+//   wrapper.style.display = show ? "none" : "block";
+// }
+
+function showQuote(quote) {
+  const {
+    author = "Unknown",
+    content = "No content available",
+    dateAdded = "",
+    dateModified = "",
+    tags = [],
+  } = quote;
+
+  wrapper.innerHTML = `
+    <div class="quotes">
+      <p class="quote"><strong>Author:</strong> ${author}</p>
+      <p class="quote"><strong>Quote:</strong> ${content}</p>
+      <p class="quote"><strong>Added:</strong> ${dateAdded.slice(0, 10)}</p>
+      <p class="quote"><strong>Modified:</strong> ${dateModified.slice(0, 10)}</p>
+      <p class="quote"><strong>Tag:</strong> ${tags[0] || "None"}</p>
+    </div>
+  `;
 }
