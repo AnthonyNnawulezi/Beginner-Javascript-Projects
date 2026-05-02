1ef519a7bcea5fa8cead6ecac6700c9f7b28cc7b
Anthony <evansanton58@gmail.com>
2026-05-02 20:23:41 +0100

    commented container as it was not needed

--- Custom Tabs/1.Custom Tabs/script1.js
+++ Custom Tabs/1.Custom Tabs/script1.js
@@ -2,18 +2,18 @@ const container = document.querySelector(".container");
 const contents = document.querySelectorAll(".content");
 const tabs = document.querySelectorAll(".tab-button");
 
-if (!container) {
-  throw new Error(
-    "Tab container not found. Ensure an element with class 'container' exists.",
-  );
-}
+// if (!container) {
+//   throw new Error(
+//     "Tab container not found. Ensure an element with class 'container' exists.",
+//   );
+// }
 
 container.addEventListener("click", (event) => {
   const clickedTab = event.target.closest(".tab-button");
   if (!clickedTab) return;
 
   const currentTabId = clickedTab.dataset.id;
-  if (!currentId) return;
+  if (!currentTabId) return;
 
   const targetContent = document.getElementById(currentTabId);
   if (!targetContent) return;
