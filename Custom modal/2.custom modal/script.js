8315d9269f72d18fe9eb4e3b4bd9b2fc5b8dac44
Anthony <evansanton58@gmail.com>
2026-05-03 23:19:57 +0100

    script.js 2nd version

---
+++ Custom modal/2.custom modal/script.js
@@ -0,0 +1,17 @@
+const openModal = document.querySelector(".open-modal");
+const container = document.querySelector(".container");
+const closeModal = document.querySelector("close-modal");
+const closeBtn = document.querySelector("close-btn");
+
+openModal.addEventListener("click", () => {
+  container.style.display = "block";
+});
+closeModal.addEventListener("click", () => {
+  container.style.display = "none";
+});
+closeBtn.addEventListener("click", () => {
+  container.style.display = "none";
+});
+window.addEventListener("click", () => {
+  container.style.display = "none";
+});
