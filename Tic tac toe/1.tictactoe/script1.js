ca8da5e7583a7781eb549c4fb6d30eadecdd2d7a
Anthony <evansanton58@gmail.com>
2026-05-05 15:16:48 +0100

    initialised state, and replaced with a simple string,instead of players array

--- Tic tac toe/1.tictactoe/script1.js
+++ Tic tac toe/1.tictactoe/script1.js
@@ -1,3 +1,6 @@
 const squares = document.querySelectorAll(".square");
 const message = document.querySelector(".message");
 const restartButton = document.querySelector(".restart-button");
+
+let currentPlayer = "X";
+let gameOver = false;
