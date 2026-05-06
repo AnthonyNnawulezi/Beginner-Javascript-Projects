3fd98309b5350eda8f450f5dc37255efbb83f6c4
Anthony <evansanton58@gmail.com>
2026-05-06 18:48:59 +0100

    scripting

---
+++ Tic tac toe/2.tictactoe/script.js
@@ -0,0 +1,55 @@
+const squares = document.querySelectorAll(".square");
+const message = document.querySelector(".message");
+const restartButton = document.querySelector(".restart-button");
+const board = document.querySelector(".board");
+
+let gameOver = false;
+let player = "X";
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function isWinner(currentPlayer) {
+  return winningCombinations.some(([a, b, c]) => {
+    return (
+      squares[a].textContent === currentPlayer &&
+      squares[b].textContent === currentPlayer &&
+      squares[c].textContent === currentPlayer
+    );
+  });
+}
+
+function setMessage(text) {
+  message.textContent = text;
+}
+
+function isDraw() {
+  if (squares.textContent !== "") return;
+}
+
+board.addEventListener("click", (e) => {
+  if (gameOver || e.target.textContent !== "") return;
+
+  e.target.textContent = player;
+
+  if (isWinner(player)) {
+    setMessage(`Player ${player} won! Please restart`);
+    gameOver = true;
+    return;
+  }
+
+  if (isDraw()) {
+    setMessage(`Its a DRAW! Please restart`);
+    gameOver = true;
+    return;
+  }
+  player = player === "X" ? "O" : "X";
+});
