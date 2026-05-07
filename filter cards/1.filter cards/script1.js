6f788404f111bd9d6e8d73c1e9695803524094c5
Anthony <evansanton58@gmail.com>
2026-05-07 21:18:20 +0100

    added null check for missing dom elements

--- filter cards/1.filter cards/script1.js
+++ filter cards/1.filter cards/script1.js
@@ -6,14 +6,27 @@ const PRODUCTS = [
   { category: "Men", label: "Men Shirt 3" },
   { category: "Men", label: "Men Shirt 4" },
   { category: "Men", label: "Men Shirt 5" },
+  { category: "Men", label: "Men Shirt 6" },
+  { category: "Men", label: "Men Shirt 7" },
   { category: "Women", label: "Women Shirt 1" },
   { category: "Women", label: "Women Shirt 2" },
   { category: "Women", label: "Women Shirt 3" },
   { category: "Women", label: "Women Shirt 4" },
   { category: "Women", label: "Women Shirt 5" },
+  { category: "Women", label: "Women Shirt 6" },
+  { category: "Women", label: "Women Shirt 7" },
   { category: "Kids", label: "Kids Shirt 1" },
   { category: "Kids", label: "Kids Shirt 2" },
   { category: "Kids", label: "Kids Shirt 3" },
   { category: "Kids", label: "Kids Shirt 4" },
   { category: "Kids", label: "Kids Shirt 5" },
+  { category: "Kids", label: "Kids Shirt 6" },
+  { category: "Kids", label: "Kids Shirt 7" },
 ];
+
+const buttonContainer = document.querySelector(".button-container");
+const contentContainer = document.querySelector(".content");
+
+if (!buttonContainer || !contentContainer) {
+  throw new Error("Missing DOM Elements not found.");
+}
