eb61e78870bd8e4f235f6603c1346fd2dec7b8e8
antonsprime <evansanton58@gmail.com>
2026-06-17 19:59:52 +0100

    replaced styling

--- ripple-effect/script.js
+++ ripple-effect/script.js
@@ -1,18 +1,1 @@
-const rippleButton = document.querySelector(".ripple-effect");
-
-rippleButton.addEventListener("click", createRippleEffect);
-
-function createRippleEffect(e) {
-  const button = e.target.closest(".ripple-effect");
-  if (!button) return;
-
-  const rect = button.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
-
-  const rippleEffect = document.createElement("span");
-  rippleEffect.style.left = `${x}px`;
-  rippleEffect.style.top = `${y}px`;
-
-  button.appendChild(rippleEffect);
-}
+const rippleButtons = document.querySelectorAll(".ripple-effect");
