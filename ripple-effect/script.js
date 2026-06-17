9fabab1e13fb203b9a149deb899dec8e4d2924d7
antonsprime <evansanton58@gmail.com>
2026-06-17 21:49:34 +0100

    finsliase

--- ripple-effect/script.js
+++ ripple-effect/script.js
@@ -1,11 +1,11 @@
 const rippleButtons = document.querySelectorAll(".ripple-effect");
 
 document.body.addEventListener("click", (event) => {
-  const target = event.target.closest(".ripple-effect");
-  if (!target) return;
+  const button = event.target.closest(".ripple-effect");
+  if (!button) return;
 
   // Compute click position relative to the target
-  const rect = target.getBoundingClientRect();
+  const rect = button.getBoundingClientRect();
   const x = event.clientX - rect.left;
   const y = event.clientY - rect.top;
 
