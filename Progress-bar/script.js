d3a05f9b4342e86706670656a1442c4a62267a69
antonsprime <evansanton58@gmail.com>
2026-06-20 19:25:30 +0100

    next and previous button functionality

--- Progress-bar/script.js
+++ Progress-bar/script.js
@@ -1,3 +1,27 @@
-const nextButton = document.query(".previous");
-const previousButton = document.query(".next");
-const iconWrapper = document.query(".icon-wrapper");
+const nextButton = document.querySelector(".previous");
+const previousButton = document.querySelector(".next");
+const icons = document.querySelectorAll(".icon-wrapper");
+
+let currentStep = 1;
+
+nextButton.addEventListener("click", () => {
+  if (currentStep < icons.length) {
+    currentStep++;
+  }
+  updateUI();
+});
+
+previousButton.addEventListener("click", () => {
+  if (currentStep > 1) {
+    currentStep--;
+  }
+  updateUI();
+});
+
+function updateUI() {
+  icons.forEach((icon, i) => {
+    if (i < currentStep) {
+      icon.classList.toggle("active", i < currentStep);
+    }
+  });
+}
