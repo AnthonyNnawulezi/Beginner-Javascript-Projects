582ab5e7c1fff5ac8ac2a7cda15337bdd428e8b0
antonsprime <evansanton58@gmail.com>
2026-06-27 21:09:32 +0100

    renamed for clearer understanding

--- ScrollTo/script.js
+++ ScrollTo/script.js
@@ -19,7 +19,7 @@ async function fetchPosts() {
 }
 
 function renderPosts(posts) {
-  const post = posts
+  const postsHTML = posts
     .map(
       ({ title, body, tags = [] }) =>
         `
@@ -32,7 +32,7 @@ function renderPosts(posts) {
     )
     .join("");
 
-  postContainer.innerHTML += post;
+  postContainer.innerHTML += postsHTML;
 }
 
 fetchPosts();
