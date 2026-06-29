b1af39dec64fb7c55064b0283368a0bc68cee3c5
antonsprime <evansanton58@gmail.com>
2026-06-29 20:50:33 +0100

    done with functionality

---
+++ Tip Calculator/script.js
@@ -0,0 +1,28 @@
+const billAmount = document.getElementById("bill-amount");
+const discountPercentage = document.getElementById("discount-percentage");
+const tipPercentage = document.getElementById("tip-percentage");
+const noOfCustormers = document.getElementById("no-of-customers");
+const totalDiscountPercentage = document.getElementById(
+  "total-discount-percentage",
+);
+const totalTipPercentage = document.getElementById("total-tip-percentage");
+const totalNoOfCustormers = document.getElementById("total-no-of-custormers");
+const totalTip = document.getElementById("total-tip");
+const totalAmount = document.getElementById("total-amount");
+const custormerToPay = document.getElementById("each-custormer-to-pay");
+
+function generateBill() {
+  const discount = (discountPercentage.value / 100).toFixed(0, 2);
+  const totalAmountToPay = (billAmount.value * discount).toFixed(0, 2);
+  const eachCustormerToPay = (totalAmount / noOfCustormers.value).toFixed(0, 2);
+  const tipAmount = (tipPercentage / 100).toFixed(0, 2);
+
+  console.log();
+
+  totalDiscountPercentage.textContent = discount;
+  totalTipPercentage.textContent = tipAmount;
+  totalNoOfCustormers.textContent = totalNoOfCustormers.value;
+  totalNoOfCustormers.textContent = totalNoOfCustormers.value;
+  totalAmount.textContent = totalAmountToPay;
+  custormerToPay.textContent = eachCustormerToPay;
+}
