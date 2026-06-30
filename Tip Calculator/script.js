a36d11f2284be3490b73b6e591e682b8c2f0897a
antonsprime <evansanton58@gmail.com>
2026-06-30 23:10:14 +0100

    bugfix

--- Tip Calculator/script.js
+++ Tip Calculator/script.js
@@ -1,29 +1,29 @@
 const billAmount = document.getElementById("bill-amount");
 const discountPercentage = document.getElementById("discount-percentage");
 const tipPercentage = document.getElementById("tip-percentage");
-const noOfCustormer = document.getElementById("no-of-customers");
+const noOfCustomers = document.getElementById("no-of-customers");
 const totalDiscountPercentage = document.getElementById(
   "total-discount-percentage",
 );
 const totalTipPercentage = document.getElementById("total-tip-percentage");
-const totalNoOfCustormer = document.getElementById("total-no-of-custormer");
+const totalNoOfCustomers = document.getElementById("total-no-of-customers");
 const totalTip = document.getElementById("total-tip");
 const totalAmount = document.getElementById("total-amount");
-const custormerToPay = document.getElementById("each-custormer-to-pay");
+const customersToPay = document.getElementById("each-customers-to-pay");
 const generateBillButton = document.getElementById("generate-bill");
 
 function generateBill() {
   const discount = discountPercentage.value;
   const totalAmountToPay = (billAmount.value * (1 - discount / 100)).toFixed(2);
-  const eachCustormerToPay = (billAmount.value / noOfCustormer.value).toFixed(
+  const eachCustomersToPay = (billAmount.value / noOfCustomers.value).toFixed(
     2,
   ); //bugfix
   const tipAmount = (tipPercentage.value / 100).toFixed(2); // bugfix
 
   totalDiscountPercentage.textContent = discount;
   totalTipPercentage.textContent = tipAmount * billAmount.value;
-  totalNoOfCustormer.textContent = noOfCustormer.value;
+  totalNoOfCustomers.textContent = noOfCustomers.value;
   totalAmount.textContent = totalAmountToPay;
-  custormerToPay.textContent = eachCustormerToPay;
+  customerToPay.textContent = eachCustomerToPay;
 }
 generateBillButton.addEventListener("click", generateBill);
