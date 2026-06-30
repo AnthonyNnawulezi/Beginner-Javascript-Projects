a67e14eb573aeedbfb09aa912d9fd9329537029e
antonsprime <evansanton58@gmail.com>
2026-06-30 22:42:14 +0100

    fix to custormer for best practice

--- Tip Calculator/script.js
+++ Tip Calculator/script.js
@@ -1,12 +1,12 @@
 const billAmount = document.getElementById("bill-amount");
 const discountPercentage = document.getElementById("discount-percentage");
 const tipPercentage = document.getElementById("tip-percentage");
-const noOfCustormers = document.getElementById("no-of-customers");
+const noOfCustormer = document.getElementById("no-of-customers");
 const totalDiscountPercentage = document.getElementById(
   "total-discount-percentage",
 );
 const totalTipPercentage = document.getElementById("total-tip-percentage");
-const totalNoOfCustormers = document.getElementById("total-no-of-custormers");
+const totalNoOfCustormer = document.getElementById("total-no-of-custormer");
 const totalTip = document.getElementById("total-tip");
 const totalAmount = document.getElementById("total-amount");
 const custormerToPay = document.getElementById("each-custormer-to-pay");
@@ -15,14 +15,14 @@ const generateBillButton = document.getElementById("generate-bill");
 function generateBill() {
   const discount = discountPercentage.value;
   const totalAmountToPay = (billAmount.value * discount).toFixed(2); // bugfix
-  const eachCustormerToPay = (billAmount.value / noOfCustormers.value).toFixed(
+  const eachCustormerToPay = (billAmount.value / noOfCustormer.value).toFixed(
     2,
   ); //bugfix
   const tipAmount = (tipPercentage.value / 100).toFixed(2); // bugfix
 
   totalDiscountPercentage.textContent = discount;
   totalTipPercentage.textContent = tipAmount * billAmount.value;
-  totalNoOfCustormers.textContent = noOfCustormers.value;
+  totalNoOfCustormer.textContent = noOfCustormer.value;
   totalAmount.textContent = totalAmountToPay;
   custormerToPay.textContent = eachCustormerToPay;
 }
