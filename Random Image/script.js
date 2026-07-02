389bd470238d852080032d45235d7150852cc9ec
antonsprime <evansanton58@gmail.com>
2026-07-03 00:32:45 +0100

    Renamed from fetchRandomImage — loadImages, Uses DocumentFragment to minimise reflows for performance

--- Random Image/script.js
+++ Random Image/script.js
@@ -4,7 +4,9 @@ const container = document.querySelector(".container");
 const IMAGES_PER_LOAD = 5;
 let currentIndex = 1;
 
-function fetchRandomImage() {
+function loadImages() {
+  const fragment = document.createDocumentFragment();
+
   for (let i = 1; i <= count + 4; i++) {
     const imageEl = document.createElement("img");
     imageEl.src = `https://picsum.photos/200/300?random=${i}`;
